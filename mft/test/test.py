2ef950d75b65757448c7cd8ab3d26aab3c673fa6
Roya-Mirhosseini <royamirhoseini95@gmail.com>
2024-07-08 19:45:54 +0330

    test

--- mft/test/test.py
+++ mft/test/test.py
@@ -1,6 +1,11 @@
 from datetime import date, datetime
-
-from mft.model.entity.student import Student
+#
+# from mft.model.entity.student import Student
 from mft.model.tools.validator import *
-student = Student("roya","mirhosseini","amir","0018453139","master","electrical engineering","17","09128404487","royamirhoseini95@gmail.com")
-print(student)
+# student = Student("roya","mirhosseini","amir","0018453139","master","electrical engineering","17","09128404487","royamirhoseini95@gmail.com")
+# print(student)
+
+
+from mft.model.entity.course import Course
+course = Course("56749","electronic 2","theoretical",3,"electronic 1","english","in person","2024/09/09","2025/03/05")
+print(course)
