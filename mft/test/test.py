bb68c4a7645c981d0aab22db32c58f02632e7351
Roya-Mirhosseini <royamirhoseini95@gmail.com>
2024-07-08 19:28:18 +0330

    test

---
+++ mft/test/test.py
@@ -0,0 +1,6 @@
+from datetime import date, datetime
+
+from mft.model.entity.student import Student
+from mft.model.tools.validator import *
+student = Student("roya","mirhosseini","amir","0018453139","master","electrical engineering","17","09128404487","royamirhoseini95@gmail.com")
+print(student)
